ceb11ae1d7148d3bf73721a5d4d91188a5cf8abb
ShadowPC <victormoralesblanco147@gmail.com>
2025-04-09 10:58:45 -0400

    Add test setup for YouTube comments and login automation
    Introduced WebDriver and WebDriverWait setup in Comments and Login test classes to automate browser actions. Updated the project language level to JDK 23 in configuration for compatibility.

--- src/test/java/Comments.java
+++ src/test/java/Comments.java
@@ -1,2 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class Comments {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeTest
+    public void setup() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
+        driver.get("https://www.youtube.com/watch?v=Q3boOPiTFHY");
+    }
+
+    //Needs to login
+    @Test(description = "Checks comments work properly")
+    public void addComment() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-comments/ytd-item-section-renderer/div[1]/ytd-comments-header-renderer/div[5]/ytd-comment-simplebox-renderer/div[3]/ytd-comment-dialog-renderer/ytd-commentbox/div[2]/div/div[2]/tp-yt-paper-input-container/div[2]/div/div[1]/ytd-emoji-input/yt-user-mention-autosuggest-input/yt-formatted-string/div")));
+        WebElement commentBox = driver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-comments/ytd-item-section-renderer/div[1]/ytd-comments-header-renderer/div[5]/ytd-comment-simplebox-renderer/div[3]/ytd-comment-dialog-renderer/ytd-commentbox/div[2]/div/div[2]/tp-yt-paper-input-container/div[2]/div/div[1]/ytd-emoji-input/yt-user-mention-autosuggest-input/yt-formatted-string/div"));
+        commentBox.sendKeys("Automation Working");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
 }
--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,2 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
 public class Login {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeTest
+    public void setup() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
+        driver.get("https://www.youtube.com/");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
 }
