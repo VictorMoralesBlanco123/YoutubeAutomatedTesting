d8b4d59f5da607cda1469b695b7726a20a6b8b44
ShadowPC <victormoralesblanco147@gmail.com>
2025-04-10 14:43:22 -0400

    added login in setup before going to video to test comment features

--- src/test/java/Comments.java
+++ src/test/java/Comments.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -11,15 +12,33 @@ import org.testng.annotations.Test;
 public class Comments {
     WebDriver driver;
     WebDriverWait wait;
+    String correctEmail = "fgcuswtspring2025@gmail.com";
+    String correctPassword = "TestP@ssword123";
 
     @BeforeTest
     public void setup() {
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://www.youtube.com");
+
+        driver.findElement(By.xpath("//a[@aria-label='Sign in']")).click();
+        driver.findElement(By.id("identifierId")).sendKeys(correctEmail);
+        Wait(3);
+        driver.findElement(By.id("identifierNext")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("Passwd")));
+        driver.findElement(By.name("Passwd")).sendKeys(correctPassword);
+        Wait(3);
+        driver.findElement(By.id("passwordNext")).click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@id='avatar-btn']")));
+        Assert.assertTrue(driver.findElement(By.id("avatar-btn")).isDisplayed(), "Login failed with valid credentials.");
+        Wait(3);
+
         driver.get("https://www.youtube.com/watch?v=Q3boOPiTFHY");
     }
 
-    //Needs to login
     @Test(description = "Checks comments work properly")
     public void addComment() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-comments/ytd-item-section-renderer/div[1]/ytd-comments-header-renderer/div[5]/ytd-comment-simplebox-renderer/div[3]/ytd-comment-dialog-renderer/ytd-commentbox/div[2]/div/div[2]/tp-yt-paper-input-container/div[2]/div/div[1]/ytd-emoji-input/yt-user-mention-autosuggest-input/yt-formatted-string/div")));
